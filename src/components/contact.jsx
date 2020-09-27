3db8a1edb16dfcdf0428637343ab2d2f5048b1cd
Loren Erickson <lorenjerickson@gmail.com>
2020-09-26 20:38:02 -0600

    require form fields

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,15 +7,15 @@ const Contact = () => (
         <form name="contact" method="POST" data-netlify="true">
           <div className="field half first">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" required />
           </div>
           <div className="field half">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" required />
           </div>
           <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6" />
+            <textarea name="message" id="message" rows="6" required />
           </div>
           <ul className="actions">
             <li>
